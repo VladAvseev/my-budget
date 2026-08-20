6ebb9872a65464f0832d92bf0c1687584c5a25a6
Vlad Avseev <vladavseev47@gmail.com>
2026-08-20 14:32:14 +0300

    Правка импортов

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { registration } from '@/modules/_registration';
 import { AuthProvider } from '@/shared/supabase/authProvider';
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
-import { login } from './modules/_login';
-import { profile } from './modules/_profile';
-import { reports } from './modules/_reports';
-import { accumulations } from './modules/_accumulations';
-import { overview } from './modules/_overview';
-import { home } from './modules/_home';
-import { help } from './modules/_help';
-import { support } from './modules/_support';
-import { admin } from './modules/_admin';
-import { notFound } from './modules/_notFound';
+import { login } from '@/modules/_login';
+import { profile } from '@/modules/_profile';
+import { reports } from '@/modules/_reports';
+import { accumulations } from '@/modules/_accumulations';
+import { overview } from '@/modules/_overview';
+import { home } from '@/modules/_home';
+import { help } from '@/modules/_help';
+import { support } from '@/modules/_support';
+import { admin } from '@/modules/_admin';
+import { notFound } from '@/modules/_notFound';
 
 const App: React.FC = () => {
   return (
